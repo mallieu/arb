903f236e1cb050e2dd24d812fa55a73c1ae7c01c
Mallieu <mallieu@hotmail.fr>
2024-01-03 09:10:24 +0400

    Disabling update notifications

--- Divi child/functions.php
+++ Divi child/functions.php
@@ -11,4 +11,15 @@ $slug = array( 'slug' => 'nos-projets' );
 return $slug;
 }
 add_filter( 'et_project_posttype_rewrite_args', 'et_projects_custom_slug', 10, 2 );
+
+// Désactiver notifications mises à jour
+// Disable core update emails
+add_filter( 'auto_core_update_send_email', '__return_false' );
+
+// Disable plugin update emails
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+
+// Disable theme update emails
+add_filter( 'auto_theme_update_send_email', '__return_false' );
+
 ?>
